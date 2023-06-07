55db8ca4643e435a4d39f90b7131728360783102
Amitai Ben Shalom <90959307+amitaibenshalom@users.noreply.github.com>
2023-06-07 17:22:47 +0300

    Update Piezoelctric_Quartz_Notes_Player.ino

--- Piezoelctric_Quartz_Notes_Player.ino
+++ Piezoelctric_Quartz_Notes_Player.ino
@@ -1,34 +1,6 @@
-
+#include "piezo_basic.h"
 #define PIN_TONE_P 8 // one side of the piezo buzzer
 #define PIN_TONE_N 12 // other side of the piezo buzzer
-uint16_t note_time = 200; // mS 
-uint16_t p_time = 200;
-uint16_t p_between_notes_time = 20;
-uint16_t silent_song_time = 2000; // mS 
-
-// freq in hertz
-float DO = 261.63;
-float DOD = 277.18;
-float RE = 293.66;
-float RED = 311.13;
-float MI = 329.63;
-float FA = 349.23;
-float FD = 369.99;
-float SOL = 392.00;
-float SOLD = 415.30;
-float LA = 440.00;
-float LAD = 466.16;
-float CI = 493.88;
-
-float P = -1; //pause note
-float END = -2; //end of song
-
-float song1[] = {SOL,MI,MI,P,FA,RE,RE,P,DO,RE,MI,FA,SOL,SOL,SOL,P,SOL,MI,MI,P,FA,RE,RE,P,DO,MI,SOL,SOL,DO,P,P,P,RE,RE,RE,RE,RE,MI,FA,P,MI,MI,MI,MI,MI,FA,SOL,P,SOL,MI,MI,P,FA,RE,RE,P,DO,MI,SOL,SOL,DO,P,END};
-float song2[] = {DO,RE,MI,FA,SOL,LA,CI,DO,END};
-
-uint16_t calculate_half_cycle_time(float note) {
-  return static_cast<unsigned int>(1/(2*note) * 1000000);
-}
 
 void play_note(uint16_t half_cycle_time, uint16_t tone_time){
   uint16_t Current_Time = millis() ;
@@ -57,21 +29,19 @@ void play_song(float song[]) {
 }
 
 
-
 void setup() {
   pinMode(PIN_TONE_P, OUTPUT);
   digitalWrite(PIN_TONE_P, LOW) ;
   pinMode(PIN_TONE_N, OUTPUT);
   digitalWrite(PIN_TONE_N, LOW) ;
   Serial.begin(115200);
-  delay (3500);// wait to make sure serial begin 
+  delay (3000);// wait to make sure serial begin 
   Serial.println(F(__FILE__ " " __DATE__ " " __TIME__));
   Serial.println("START");
-
 }
 
 void loop() {
   Serial.println("now play");
-  play_song(song1);
+  play_song(song2);
   delay(silent_song_time);
 }
